1c78c43c0aaf1d617e23068a19a054b12774c399
jyingj <1156088591@qq.com>
2017-02-27 22:56:11 +0800

    kkkk

--- src/com/jiaozi/GitTest.java
+++ src/com/jiaozi/GitTest.java
@@ -3,7 +3,12 @@ package com.jiaozi;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("hello Git");
+		System.out.println("hello Git1");
+		System.out.println("hello Git2");
+		System.out.println("hello Git3");
+		System.out.println("hello Git4");
+		System.out.println("hello Git5");
+		
 	}
 
 }
